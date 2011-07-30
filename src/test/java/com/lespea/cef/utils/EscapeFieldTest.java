4c2957f34d4abe72a606fdc295bfe13e6eebeccb
Adam Lesperance <lespea@gmail.com>
2011-07-29 22:30:17 -0500

    Use the test helper function to simply everything

--- src/test/java/com/lespea/cef/utils/EscapeFieldTest.java
+++ src/test/java/com/lespea/cef/utils/EscapeFieldTest.java
@@ -1,18 +1,69 @@
+/**
+ * EscapeFieldTest.java    2011-07-29
+ *
+ * Copyright 2011, Adam Lesperance
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this library; if not, write to the Free Software Foundation, Inc.,
+ * 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+ */
+
+
+
 package com.lespea.cef.utils;
 
-import org.testng.annotations.Test;
+//~--- non-JDK imports --------------------------------------------------------
+
+import com.lespea.cef.InvalidField;
+
+import junit.framework.Assert;
+
 import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
 
+
+//~--- classes ----------------------------------------------------------------
+
+/**
+ * Make sure the CEF fields are properly escaped under all circumstances.
+ *
+ * @author Adam Lesperance
+ */
 public class EscapeFieldTest {
-  @Test(dataProvider = "dp")
-  public void f(Integer n, String s) {
-  }
 
-  @DataProvider
-  public Object[][] dp() {
-    return new Object[][] {
-      new Object[] { 1, "a" },
-      new Object[] { 2, "b" },
-    };
-  }
+    /**
+     * List of strings that contain pipes and how they should look after being escaped.
+     *
+     * @return the grouping of strings to process
+     */
+    @DataProvider
+    public Object[][] pipeFields() {
+        return TestHelpers.genEscapeStrings( "|", "\\|" );
+    }
+
+
+    /**
+     * Makes sure the pipe character is properly escaped in a field.
+     *
+     * @param unquotedStr
+     *            the string to quote
+     * @param quotedStr
+     *            what the string should be transformed to by the function
+     * @throws InvalidField
+     *             if the field contains an invalid character
+     */
+    @Test(dataProvider = "pipeFields")
+    public void testFieldPipes( final String unquotedStr, final String quotedStr ) throws InvalidField {
+        Assert.assertEquals( quotedStr, StringUtils.escapeField( unquotedStr ) );
+    }
 }
